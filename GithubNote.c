0afcb91c9478d6e502cbdee74eb21384ea1972dd
Kelvin Nguyen <kelvinguyen@hotmail.com>
2016-03-18 10:33:03 -0600

    initial-Commint
    only git attribute and gitignore

---
+++ GithubNote.c
@@ -0,0 +1,33 @@
+// What is git?
+--> version control system
+
+--> Basix Git Commands
+	- init : use to create a repository
+	- status : current state of your local repository, none commit change
+	- add : add file to the local repository
+	- commit : how you commiting to your local repository
+	- log : how you see change that have been committed
+	- push : is the command to push your change that you have commit locally up to the 		 central repository
+	- pull : pull change down from other repository
+	- merge : merge change that come in from separate places , branches
+
+--> Git command demo
+	// clone a project from github to your local repository
+	> git clone https://github.com/brendanPluralsight/cdm-demo.git
+
+//Github
+--> What is Github?
+	- it's a server/hub for hosting Git-based project
+--> Setup Github
+	- GoTo: https://github.com
+	- download set-it-up for window version for local Github IDE
+	- create account on github website
+		--> create a repository
+			- give name to your repository
+			- add gitignore specify to what programing language
+			- add license
+
+--> account and organization
+	- account : individual user
+	- organization : a group of account
+	--> convert your account to organization or create organization in account setting
